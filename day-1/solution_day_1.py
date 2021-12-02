4987060e92c9e718f0619d08ed445abde53e731e
jszafran <jszafran.pv@gmail.com>
2021-12-02 17:45:57 +0100

    Adding initial implementation for day 1.

---
+++ day-1/solution_day_1.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+def get_input_data() -> List[int]:
+    with open("aoc-day-1-input.txt", "rt") as f:
+        data = [int(line) for line in f.readlines()]
+    return data
+
+
+def get_increased_count() -> int:
+    input_data = get_input_data()
+    index_to_value = {}
+
+    comparisons = []
+    for i, v in enumerate(input_data):
+        index_to_value[i] = v
+        if i == 0:
+            continue
+        comparisons.append(v > index_to_value[i-1])
+    return sum(comparisons)
+
+
+if __name__ == '__main__':
+    print(get_increased_count())
