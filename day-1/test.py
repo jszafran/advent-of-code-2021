7d73a83e70dbfffa97fcd2f859a726610d063a6e
jszafran <jszafran.pv@gmail.com>
2021-12-02 17:53:34 +0100

    Rename readme file name + tests.

---
+++ day-1/test.py
@@ -0,0 +1,22 @@
+import unittest
+
+from solution_day_1 import get_increased_count
+
+
+class TestDay1Algorithm(unittest.TestCase):
+    def test_algorithm(self):
+        inp1 = [1, 1, 1, 1, 1]
+        self.assertEqual(0, get_increased_count(inp1))
+
+        inp2 = [1, 1, 1, 1, 1, 3]
+        self.assertEqual(1, get_increased_count(inp2))
+
+        inp3 = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        self.assertEqual(7, get_increased_count(inp3))
+
+        inp4 = [5,4,3,2,1]
+        self.assertEqual(0, get_increased_count(inp4))
+
+
+if __name__ == '__main__':
+    unittest.main()
