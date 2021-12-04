a19bfc0450ed26e5d3f5615240da584dc9ef456f
jszafran <jszafran.pv@gmail.com>
2021-12-04 09:49:12 +0100

    Finishing game board parser.

--- day-4/solution.py
+++ day-4/solution.py
@@ -1,7 +1,11 @@
 from dataclasses import dataclass
-from typing import List, Sequence
+from typing import List, Sequence, Tuple
 
 Row = List[int]
+
+EMPTY_LINE = "\n"
+
+BOARD_LENGTH = 5
 
 
 @dataclass(frozen=True)
@@ -32,3 +36,22 @@ class BingoBoard:
     def from_sequence_of_strings(cls, string_sequence: Sequence[str]):
         rows = [[int(x) for x in line.split(" ")] for line in string_sequence]
         return cls(rows)
+
+
+def parse_game_data(path: str) -> Tuple[List[int], List[BingoBoard]]:
+    with open(path, "rt") as f:
+        data = f.readlines()
+    drawn_numbers = [int(x) for x in data[0].split(",")]
+
+    boards = []
+    board_buffer = []
+    for line in data[1:]:
+        if line == EMPTY_LINE:
+            continue
+        board_buffer.append(line.replace("\n", "").strip().replace("  ", " "))
+        if len(board_buffer) == BOARD_LENGTH:
+            board = BingoBoard.from_sequence_of_strings(board_buffer)
+            boards.append(board)
+            board_buffer.clear()
+
+    return drawn_numbers, boards
