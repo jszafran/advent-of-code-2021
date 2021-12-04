3dd784ea506a876a90038adab6aaf26254f38eb1
jszafran <jszafran.pv@gmail.com>
2021-12-04 10:08:04 +0100

    Adding util methods for checking boards state.

--- day-4/solution.py
+++ day-4/solution.py
@@ -1,7 +1,8 @@
 from dataclasses import dataclass
-from typing import List, Sequence, Tuple
+from typing import List, Sequence, Set, Tuple
 
 Row = List[int]
+Column = List[int]
 
 EMPTY_LINE = "\n"
 
@@ -23,6 +24,10 @@ class BingoBoard:
         return len(self.rows)
 
     @property
+    def columns(self) -> List[Column]:
+        return [[row[i] for row in self.rows] for i in range(self.board_length)]
+
+    @property
     def are_numbers_unique(self) -> bool:
         """
         Making sure that all numbers in particular board are unique.
@@ -31,6 +36,20 @@ class BingoBoard:
             len(set([num for row in self.rows for num in row]))
             == self.board_length ** 2
         )
+
+    @property
+    def all_numbers(self) -> Set[int]:
+        return set([num for row in self.rows for num in row])
+
+    def get_unmarked_numbers(self, marked_numbers: Set[int]) -> Set[int]:
+        return self.all_numbers - marked_numbers
+
+    def has_won(self, drawn_numbers: Set[int]) -> bool:
+        has_winning_row = any(set(row) <= drawn_numbers for row in self.rows)
+        has_winning_column = any(
+            set(column) <= drawn_numbers for column in self.columns
+        )
+        return has_winning_row or has_winning_column
 
     @classmethod
     def from_sequence_of_strings(cls, string_sequence: Sequence[str]):
@@ -55,3 +74,9 @@ def parse_game_data(path: str) -> Tuple[List[int], List[BingoBoard]]:
             board_buffer.clear()
 
     return drawn_numbers, boards
+
+
+nums, boards = parse_game_data("input.txt")
+print(boards[0])
+print(boards[0].rows)
+print(boards[0].columns)
